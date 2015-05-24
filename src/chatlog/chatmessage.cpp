Zer0C00I/qTox

commit 8d0dec8a111713be22fb71744fb9dccce1a38de8

Don't quote with action messages
Quoting with action messages produces badly looking messages,
where nick is blue coloured, while rest of text that is preceded
by '> ' is green. To avoid that, quoting should not be triggered
in action messages.

src/chatlog/chatmessage.cpp
@@ -46,8 +46,6 @@ ChatMessage::Ptr ChatMessage::createChatMessage(const QString &sender, const QSt
     if (Settings::getInstance().getUseEmoticons())
         text = SmileyPack::getInstance().smileyfied(text);
 
-    //quotes (green text)
-    text = detectQuotes(detectAnchors(text));
 
     switch(type)
     {
@@ -57,9 +55,13 @@ ChatMessage::Ptr ChatMessage::createChatMessage(const QString &sender, const QSt
         msg->setAsAction();
         break;
     case ALERT:
+        // quotes should not be available for action messages ↑
+        text = detectQuotes(detectAnchors(text)); //quotes (green text)
         text = wrapDiv(text, "alert");
         break;
     default:
+        // quotes should not be avaialble for action messages
+        text = detectQuotes(detectAnchors(text)); //quotes (green text)
         text = wrapDiv(text, "msg");
     }
 

commit e09518794e296d33a28bd15bfaf96a9e5b2072dd

Properly fix quoting in action messages
* Allow quoting in action messages if line number is >1.
* Fix regression introduced by 8d0dec8a111713be22fb71744fb9dccce1a38de8 where
  '\n' was being trimmed in action messages.
* Add comment about typing notifications placeholder that needs to be fixed.

src/chatlog/chatmessage.cpp
@@ -46,6 +46,8 @@ ChatMessage::Ptr ChatMessage::createChatMessage(const QString &sender, const QSt
     if (Settings::getInstance().getUseEmoticons())
         text = SmileyPack::getInstance().smileyfied(text);
 
+    //quotes (green text)
+    text = detectQuotes(detectAnchors(text), type);
 
     switch(type)
     {
@@ -55,13 +57,9 @@ ChatMessage::Ptr ChatMessage::createChatMessage(const QString &sender, const QSt
         msg->setAsAction();
         break;
     case ALERT:
-        // quotes should not be available for action messages ↑
-        text = detectQuotes(detectAnchors(text)); //quotes (green text)
         text = wrapDiv(text, "alert");
         break;
     default:
-        // quotes should not be avaialble for action messages
-        text = detectQuotes(detectAnchors(text)); //quotes (green text)
         text = wrapDiv(text, "msg");
     }
 
@@ -112,6 +110,11 @@ ChatMessage::Ptr ChatMessage::createTypingNotification()
     ChatMessage::Ptr msg = ChatMessage::Ptr(new ChatMessage);
 
     // Note: "[user]..." is just a placeholder. The actual text is set in ChatForm::setFriendTyping()
+    //
+    // FIXME: Due to circumstances, placeholder is being used in a case where
+    // user received typing notifications constantly since contact came online.
+    // This causes "[user]..." to be displayed in place of user nick, as long
+    // as user will keep typing. Issue #1280
     msg->addColumn(new NotificationIcon(QSize(18, 18)), ColumnFormat(NAME_COL_WIDTH, ColumnFormat::FixedSize, ColumnFormat::Right));
     msg->addColumn(new Text("[user]...", Style::getFont(Style::Big), false, ""), ColumnFormat(1.0, ColumnFormat::VariableSize, ColumnFormat::Left));
 
@@ -198,17 +201,26 @@ QString ChatMessage::detectAnchors(const QString &str)
     return out;
 }
 
-QString ChatMessage::detectQuotes(const QString& str)
+QString ChatMessage::detectQuotes(const QString& str, MessageType type)
 {
     // detect text quotes
     QStringList messageLines = str.split("\n");
     QString quotedText;
-    for (int i=0;i<messageLines.size();++i)
+    for (int i = 0; i < messageLines.size(); ++i)
     {
-        if (QRegExp("^(&gt;|＞)( |[[]|&gt;|[^_\\d\\W]).*").exactMatch(messageLines[i]))
-            quotedText += "<span class=quote>" + messageLines[i] + "</span>";
-        else
+        // don't quote first line in action message. This makes co-existence of
+        // quotes and action messages possible, since only first line can cause
+        // problems in case where there is quote in it used.
+        if (QRegExp("^(&gt;|＞)( |[[]|&gt;|[^_\\d\\W]).*").exactMatch(messageLines[i])) {
+            if (type != ACTION)
+                quotedText += "<span class=quote>" + messageLines[i] + "</span>";
+            else if (type == ACTION && i > 0)
+                quotedText += "<span class=quote>" + messageLines[i] + "</span>";
+            else if (type == ACTION && i == 0)
+                quotedText += messageLines[i];
+        } else {
             quotedText += messageLines[i];
+        }
 
         if (i < messageLines.size() - 1)
             quotedText += "<br/>";

src/chatlog/chatmessage.h
@@ -57,7 +57,7 @@ public:
 
 protected:
     static QString detectAnchors(const QString& str);
-    static QString detectQuotes(const QString& str);
+    static QString detectQuotes(const QString& str, MessageType type);
     static QString wrapDiv(const QString& str, const QString& div);
 
 private: