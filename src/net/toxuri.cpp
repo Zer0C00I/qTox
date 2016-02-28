Zer0C00I/qTox

commit fecfa84a92043a6752c331aa4cbb9bedb7471350

fix uri parsing and tox URI detection, closes #2118

src/net/toxuri.cpp
@@ -57,7 +57,7 @@ bool handleToxURI(const QString &toxURI)
         qApp->processEvents();
 
     QString toxaddr;
-    if (toxURI.startsWith("tox://"))
+    if (toxURI.startsWith("tox:"))
         toxaddr = toxURI.mid(6);
     else
         toxaddr = toxURI.mid(4);
@@ -66,11 +66,13 @@ bool handleToxURI(const QString &toxURI)
 
     if (toxid.isEmpty())
     {
-        QMessageBox::warning(0, "qTox", toxaddr+" is not a valid Tox address.");
+        QMessageBox::warning(0, "qTox", toxaddr + " is not a valid Tox address.");
     }
     else
     {
-        ToxURIDialog dialog(0, toxaddr, QObject::tr("%1 here! Tox me maybe?","Default message in Tox URI friend requests. Write something appropriate!").arg(Nexus::getCore()->getUsername()));
+        ToxURIDialog dialog(0, toxaddr, QObject::tr("%1 here! Tox me maybe?",
+                                                    "Default message in Tox URI friend requests. Write something appropriate!")
+                            .arg(Nexus::getCore()->getUsername()));
         if (dialog.exec() == QDialog::Accepted)
             Core::getInstance()->requestFriendship(toxid, dialog.getRequestMessage());
     }
@@ -81,7 +83,7 @@ ToxURIDialog::ToxURIDialog(QWidget *parent, const QString &userId, const QString
     QDialog(parent)
 {
     setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
-    setWindowTitle(tr("Add a friend","Title of the window to add a friend through Tox URI"));
+    setWindowTitle(tr("Add a friend", "Title of the window to add a friend through Tox URI"));
 
     QLabel *friendsLabel = new QLabel(tr("Do you want to add %1 as a friend?").arg(userId), this);
     QLabel *userIdLabel = new QLabel(tr("User ID:"), this);
@@ -93,8 +95,8 @@ ToxURIDialog::ToxURIDialog(QWidget *parent, const QString &userId, const QString
 
     QDialogButtonBox *buttonBox = new QDialogButtonBox(Qt::Horizontal, this);
 
-    buttonBox->addButton(tr("Send","Send a friend request"), QDialogButtonBox::AcceptRole);
-    buttonBox->addButton(tr("Cancel","Don't send a friend request"), QDialogButtonBox::RejectRole);
+    buttonBox->addButton(tr("Send", "Send a friend request"), QDialogButtonBox::AcceptRole);
+    buttonBox->addButton(tr("Cancel", "Don't send a friend request"), QDialogButtonBox::RejectRole);
 
     connect(buttonBox, &QDialogButtonBox::accepted, this, &FriendRequestDialog::accept);
     connect(buttonBox, &QDialogButtonBox::rejected, this, &FriendRequestDialog::reject);

commit 5aa2b29df1653096d8455082a01182159a8bdad8

Fix toxURI parsing.
It was always eating two characters more than needed.

A toxURI passed to handleToxURI always starts with "tox:", it's checked in the handleToxURI callers. So here we need to remove that prefix, that's long 4 characters and not 6.

The previous code was meant to handle tox:// and tox: cases, but the tox:// case is invalid and should not be handled (see #2118).

Fixes: fecfa84a92043a6752c331aa4cbb9bedb7471350 ("fix uri parsing and tox URI detection, closes #2118")

src/net/toxuri.cpp
@@ -57,11 +57,7 @@ bool handleToxURI(const QString &toxURI)
     while (!core->isReady())
         qApp->processEvents();
 
-    QString toxaddr;
-    if (toxURI.startsWith("tox:"))
-        toxaddr = toxURI.mid(6);
-    else
-        toxaddr = toxURI.mid(4);
+    QString toxaddr = toxURI.mid(4);
 
     QString toxId = Toxme::lookup(toxaddr).toString();
     if (toxId.isEmpty())