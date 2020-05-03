Zer0C00I/qTox

commit 4cb9bf0e8017c29f62a7013c361296fcb57fe493

feat(toxid): reduce passing Tox IDs around
reasons:
- most of the time we don't even know the Tox ID but only the Public Key
- use well defined objects instead of strings that could be anything

src/core/toxid.cpp
@@ -20,6 +20,7 @@
 
 #include "toxid.h"
 #include "core.h"
+#include "toxpk.h"
 
 #include <tox/tox.h>
 
@@ -201,27 +202,9 @@ const uint8_t* ToxId::getBytes() const
  * @brief Gets the Public Key part of the ToxID
  * @return Public Key of the ToxID
  */
-QByteArray ToxId::getPublicKey() const
+ToxPk ToxId::getPublicKey() const
 {
-    return toxId.mid(0, TOX_PUBLIC_KEY_SIZE);
-}
-
-/**
- * @brief Gets the Public Key part of the ToxID, convenience function for toxcore interface.
- * @return Public Key of the ToxID as uint8_t*
- */
-const uint8_t* ToxId::getPublicKeyBytes() const
-{
-    return reinterpret_cast<const uint8_t*>(toxId.mid(0, TOX_PUBLIC_KEY_SIZE).constData());
-}
-
-/**
- * @brief Returns the Public Key converted to QString.
- * @return The Public Key as QString.
- */
-QString ToxId::getPublicKeyString() const
-{
-    return getPublicKey().toHex().toUpper();
+    return ToxPk(toxId.mid(0, TOX_PUBLIC_KEY_SIZE));
 }
 
 /**

src/persistence/profile.cpp
@@ -417,7 +417,7 @@ QString Profile::avatarPath(const QString& ownerId, bool forceUnencrypted)
         return Settings::getInstance().getSettingsDirPath() + "avatars/" + ownerId + ".png";
 
     QByteArray idData = ownerId.toUtf8();
-    QByteArray pubkeyData = core->getSelfId().getPublicKey();
+    QByteArray pubkeyData = core->getSelfId().getPublicKey().getKey();
     constexpr int hashSize = TOX_PUBLIC_KEY_SIZE;
     static_assert(hashSize >= crypto_generichash_BYTES_MIN
                   && hashSize <= crypto_generichash_BYTES_MAX, "Hash size not supported by libsodium");
@@ -434,7 +434,7 @@ QString Profile::avatarPath(const QString& ownerId, bool forceUnencrypted)
  */
 QPixmap Profile::loadAvatar()
 {
-    return loadAvatar(core->getSelfId().getPublicKeyString());
+    return loadAvatar(core->getSelfId().getPublicKey().getKey());
 }
 
 /**
@@ -495,7 +495,7 @@ QByteArray Profile::loadAvatarData(const QString& ownerId, const QString& passwo
     return pic;
 }
 
-void Profile::loadDatabase(const QString& id)
+void Profile::loadDatabase(const ToxId& id)
 {
     if(isRemoved)
     {
@@ -503,7 +503,7 @@ void Profile::loadDatabase(const QString& id)
         return;
     }
 
-    QByteArray salt = ToxId {id}.getPublicKey();
+    QByteArray salt = id.getPublicKey().getKey();
     if(salt.size() != TOX_PASS_SALT_LENGTH)
     {
         qWarning() << "Couldn't compute salt from public key" << name;
@@ -572,7 +572,7 @@ QByteArray Profile::getAvatarHash(const QString& ownerId)
  */
 void Profile::removeAvatar()
 {
-    removeAvatar(core->getSelfId().getPublicKeyString());
+    removeAvatar(core->getSelfId().getPublicKey().toString());
 }
 
 /**
@@ -600,7 +600,7 @@ History* Profile::getHistory()
 void Profile::removeAvatar(const QString& ownerId)
 {
     QFile::remove(avatarPath(ownerId));
-    if (ownerId == core->getSelfId().getPublicKeyString())
+    if (ownerId == core->getSelfId().getPublicKey().toString())
         core->setAvatar({});
 }
 
@@ -789,7 +789,7 @@ void Profile::restartCore()
  */
 void Profile::setPassword(const QString& newPassword)
 {
-    QByteArray avatar = loadAvatarData(core->getSelfId().getPublicKeyString());
+    QByteArray avatar = loadAvatarData(core->getSelfId().getPublicKey().toString());
     QString oldPassword = password;
     password = newPassword;
     passkey = core->createPasskey(password);
@@ -801,14 +801,14 @@ void Profile::setPassword(const QString& newPassword)
     }
 
     Nexus::getDesktopGUI()->reloadHistory();
-    saveAvatar(avatar, core->getSelfId().getPublicKeyString());
+    saveAvatar(avatar, core->getSelfId().getPublicKey().toString());
 
     QVector<uint32_t> friendList = core->getFriendList();
     QVectorIterator<uint32_t> i(friendList);
     while (i.hasNext())
     {
-        QString friendPublicKey = core->getFriendPublicKey(i.next());
-        saveAvatar(loadAvatarData(friendPublicKey,oldPassword),friendPublicKey);
+        QString friendPublicKey = core->getFriendPublicKey(i.next()).toString();
+        saveAvatar(loadAvatarData(friendPublicKey, oldPassword), friendPublicKey);
     }
 }
 

src/widget/widget.cpp
@@ -993,10 +993,9 @@ void Widget::reloadHistory()
     }
 }
 
-void Widget::addFriend(int friendId, const QString &userId)
+void Widget::addFriend(int friendId, const ToxPk& friendPk)
 {
-    ToxId userToxId = ToxId(userId);
-    Friend* newfriend = FriendList::addFriend(friendId, userToxId);
+    Friend* newfriend = FriendList::addFriend(friendId, friendPk);
 
     QString name = newfriend->getDisplayedName();
     FriendWidget* widget = new FriendWidget(friendId, name);
@@ -1009,14 +1008,14 @@ void Widget::addFriend(int friendId, const QString &userId)
 
     const Settings& s = Settings::getInstance();
 
-    QDate activityDate = s.getFriendActivity(userToxId);
+    QDate activityDate = s.getFriendActivity(friendPk);
     QDate chatDate = friendForm->getLatestDate();
     if (chatDate > activityDate && chatDate.isValid())
     {
-        Settings::getInstance().setFriendActivity(userToxId, chatDate);
+        Settings::getInstance().setFriendActivity(friendPk, chatDate);
     }
 
-    contactListWidget->addFriendWidget(widget, Status::Offline, s.getFriendCircleID(userToxId));
+    contactListWidget->addFriendWidget(widget, Status::Offline, s.getFriendCircleID(friendPk));
 
     connect(newfriend, &Friend::displayedNameChanged, this, &Widget::onFriendDisplayChanged);
     connect(widget, &FriendWidget::chatroomWidgetClicked, this, &Widget::onChatroomWidgetClicked);
@@ -1025,7 +1024,7 @@ void Widget::addFriend(int friendId, const QString &userId)
     connect(widget, SIGNAL(removeFriend(int)), this, SLOT(removeFriend(int)));
 
     // Try to get the avatar from the cache
-    QPixmap avatar = Nexus::getProfile()->loadAvatar(userId);
+    QPixmap avatar = Nexus::getProfile()->loadAvatar(friendPk.toString());
     if (!avatar.isNull())
     {
         friendForm->onAvatarChange(friendId, avatar);
@@ -1036,7 +1035,7 @@ void Widget::addFriend(int friendId, const QString &userId)
     widget->search(ui->searchContactText->text(), filterOffline(filter));
 }
 
-void Widget::addFriendFailed(const QString&, const QString& errorInfo)
+void Widget::addFriendFailed(const ToxPk&, const QString& errorInfo)
 {
     QString info = QString(tr("Couldn't request friendship"));
     if (!errorInfo.isEmpty())
@@ -1226,7 +1225,7 @@ void Widget::onFriendMessageReceived(int friendId, const QString& message, bool 
     Profile* profile = Nexus::getProfile();
     if (profile->isHistoryEnabled())
     {
-        QString publicKey = f->getToxId().getPublicKeyString();
+        QString publicKey = f->getPublicKey().toString();
         QString name = f->getDisplayedName();
         QString text = message;
         if (isAction)
@@ -1278,7 +1277,7 @@ void Widget::addFriendDialog(Friend *frnd, ContentDialog *dialog)
     connect(core, &Core::friendAvatarRemoved,
             friendWidget, &FriendWidget::onAvatarRemoved);
 
-    QPixmap avatar = Nexus::getProfile()->loadAvatar(frnd->getToxId().toString());
+    QPixmap avatar = Nexus::getProfile()->loadAvatar(frnd->getPublicKey().toString());
     if (!avatar.isNull())
     {
         friendWidget->onAvatarChange(frnd->getFriendID(), avatar);
@@ -1469,9 +1468,9 @@ bool Widget::newMessageAlert(QWidget* currentWindow, bool isActive, bool sound, 
     return true;
 }
 
-void Widget::onFriendRequestReceived(const QString& userId, const QString& message)
+void Widget::onFriendRequestReceived(const ToxPk& friendPk, const QString& message)
 {
-    if (addFriendForm->addFriendRequest(userId, message))
+    if (addFriendForm->addFriendRequest(friendPk.toString(), message))
     {
         friendRequestsUpdate();
         newMessageAlert(window(), isActiveWindow(), true, true);
@@ -1480,12 +1479,12 @@ void Widget::onFriendRequestReceived(const QString& userId, const QString& messa
 
 void Widget::updateFriendActivity(Friend *frnd)
 {
-    QDate date = Settings::getInstance().getFriendActivity(frnd->getToxId());
+    QDate date = Settings::getInstance().getFriendActivity(frnd->getPublicKey());
     if (date != QDate::currentDate())
     {
         // Update old activity before after new one. Store old date first.
-        QDate oldDate = Settings::getInstance().getFriendActivity(frnd->getToxId());
-        Settings::getInstance().setFriendActivity(frnd->getToxId(), QDate::currentDate());
+        QDate oldDate = Settings::getInstance().getFriendActivity(frnd->getPublicKey());
+        Settings::getInstance().setFriendActivity(frnd->getPublicKey(), QDate::currentDate());
         contactListWidget->moveWidget(friendWidgets[frnd->getFriendID()], frnd->getStatus());
         contactListWidget->updateActivityDate(oldDate);
     }
@@ -1505,7 +1504,7 @@ void Widget::removeFriend(Friend* f, bool fake)
 
         if (ask.removeHistory())
         {
-            Nexus::getProfile()->getHistory()->removeFriendHistory(f->getToxId().getPublicKeyString());
+            Nexus::getProfile()->getHistory()->removeFriendHistory(f->getPublicKey().toString());
         }
     }
 
@@ -1669,7 +1668,7 @@ void Widget::copyFriendIdToClipboard(int friendId)
     if (f != nullptr)
     {
         QClipboard *clipboard = QApplication::clipboard();
-        clipboard->setText(Nexus::getCore()->getFriendAddress(f->getFriendID()), QClipboard::Clipboard);
+        clipboard->setText(Nexus::getCore()->getFriendPublicKey(f->getFriendID()).toString(), QClipboard::Clipboard);
     }
 }
 
@@ -1710,8 +1709,8 @@ void Widget::onGroupMessageReceived(int groupnumber, int peernumber, const QStri
     }
 
     const Core* core = Core::getInstance();
-    ToxId author = core->getGroupPeerToxId(groupnumber, peernumber);
-    bool isSelf = author == core->getSelfId();
+    ToxPk author = core->getGroupPeerPk(groupnumber, peernumber);
+    bool isSelf = author == core->getSelfId().getPublicKey();
 
     bool targeted = !isSelf && (message.contains(nameMention) || message.contains(sanitizedNameMention));
     if (targeted && !isAction)

commit f5cd7399548d1547d2813eb761bd6f69c86c01c5

refactor(history): enforce using PK instead of ToxId in history
We need to be lenient when reading from db because of ToxIds being saved in the
db from a bug introduced in 4cb9bf0e8017c29f62a7013c361296fcb57fe493, which
used self ID rather than self Pk. Was subsequently fixed in
a3443535257d8ad8f8beecaa8bc0e807d64627ff.

src/core/toxid.cpp
@@ -114,7 +114,7 @@ ToxId::ToxId(const QByteArray& rawId)
  * If the given rawId isn't a valid Public Key or Tox ID a ToxId with all zero bytes is created.
  *
  * @param rawId Pointer to bytes to convert to ToxId object
- * @param len Number of bytes to read. Must be TOX_SECRET_KEY_SIZE for a Public Key or
+ * @param len Number of bytes to read. Must be TOX_PUBLIC_KEY_SIZE for a Public Key or
  *            TOX_ADDRESS_SIZE for a Tox ID.
  */
 ToxId::ToxId(const uint8_t* rawId, int len)
@@ -127,7 +127,7 @@ ToxId::ToxId(const uint8_t* rawId, int len)
 void ToxId::constructToxId(const QByteArray& rawId)
 {
     // TODO: remove construction from PK only
-    if (rawId.length() == TOX_SECRET_KEY_SIZE) {
+    if (rawId.length() == TOX_PUBLIC_KEY_SIZE) {
         toxId = QByteArray(rawId); // construct from PK only
     } else if (rawId.length() == TOX_ADDRESS_SIZE && isToxId(rawId.toHex().toUpper())) {
         toxId = QByteArray(rawId); // construct from full toxid

src/model/about/aboutfriend.cpp
@@ -123,7 +123,7 @@ bool AboutFriend::clearHistory()
     const ToxPk pk = f->getPublicKey();
     History* const history = Nexus::getProfile()->getHistory();
     if (history) {
-        history->removeFriendHistory(pk.toString());
+        history->removeFriendHistory(pk);
         return true;
     }
 

src/model/chathistory.cpp
@@ -155,7 +155,7 @@ SearchResult ChatHistory::searchBackward(SearchPos startIdx, const QString& phra
     // If the double disk access is real bad we can optimize this by adding
     // another function to history
     auto dateWherePhraseFound =
-        history->getDateWhereFindPhrase(f.getPublicKey().toString(), earliestMessageDate, phrase,
+        history->getDateWhereFindPhrase(f.getPublicKey(), earliestMessageDate, phrase,
                                         parameter);
 
     if (dateWherePhraseFound.isValid()) {
@@ -219,8 +219,8 @@ void ChatHistory::onFileUpdated(const ToxPk& sender, const ToxFile& file)
             // chat log. Both rely on generating a new id based on the state of
             // initializing. If this is changed in the session chat log we'll end up
             // with a different order when loading from history
-            history->addNewFileMessage(f.getPublicKey().toString(), file.resumeFileId, file.fileName,
-                                       file.filePath, file.filesize, sender.toString(),
+            history->addNewFileMessage(f.getPublicKey(), file.resumeFileId, file.fileName,
+                                       file.filePath, file.filesize, sender,
                                        QDateTime::currentDateTime(), f.getDisplayedName());
             break;
         }
@@ -256,7 +256,7 @@ void ChatHistory::onFileTransferBrokenUnbroken(const ToxPk& sender, const ToxFil
 void ChatHistory::onMessageReceived(const ToxPk& sender, const Message& message)
 {
     if (canUseHistory()) {
-        auto friendPk = f.getPublicKey().toString();
+        auto friendPk = f.getPublicKey();
         auto displayName = f.getDisplayedName();
         auto content = message.content;
         if (message.isAction) {
@@ -272,8 +272,8 @@ void ChatHistory::onMessageReceived(const ToxPk& sender, const Message& message)
 void ChatHistory::onMessageSent(DispatchedMessageId id, const Message& message)
 {
     if (canUseHistory()) {
-        auto selfPk = coreIdHandler.getSelfPublicKey().toString();
-        auto friendPk = f.getPublicKey().toString();
+        auto selfPk = coreIdHandler.getSelfPublicKey();
+        auto friendPk = f.getPublicKey();
 
         auto content = message.content;
         if (message.isAction) {

src/persistence/history.cpp
@@ -24,6 +24,7 @@
 #include "profile.h"
 #include "settings.h"
 #include "db/rawdatabase.h"
+#include "src/core/toxpk.h"
 
 namespace {
 static constexpr int SCHEMA_VERSION = 4;
@@ -332,7 +333,10 @@ History::History(std::shared_ptr<RawDatabase> db_)
     // Cache our current peers
     db->execLater(RawDatabase::Query{"SELECT public_key, id FROM peers;",
                                      [this](const QVector<QVariant>& row) {
-                                         peers[row[0].toString()] = row[1].toInt();
+                                         // HACK: we previously accidentally put Tox IDs in the db. So instead of
+                                         // constructing as a ToxPk which will enforce the correct length, construct
+                                         // as ToxId which will allow either length, and then convert to ToxPk.
+                                         peers[ToxId{QByteArray::fromHex(row[0].toByteArray())}.getPublicKey()] = row[1].toInt();
                                      }});
 }
 
@@ -392,7 +396,7 @@ void History::eraseHistory()
  * @brief Erases the chat history with one friend.
  * @param friendPk Friend public key to erase.
  */
-void History::removeFriendHistory(const QString& friendPk)
+void History::removeFriendHistory(const ToxPk& friendPk)
 {
     if (!isValid()) {
         return;
@@ -441,8 +445,8 @@ void History::removeFriendHistory(const QString& friendPk)
  * @param insertIdCallback Function, called after query execution.
  */
 QVector<RawDatabase::Query>
-History::generateNewMessageQueries(const QString& friendPk, const QString& message,
-                                   const QString& sender, const QDateTime& time, bool isDelivered,
+History::generateNewMessageQueries(const ToxPk& friendPk, const QString& message,
+                                   const ToxPk& sender, const QDateTime& time, bool isDelivered,
                                    QString dispName, std::function<void(RowId)> insertIdCallback)
 {
     QVector<RawDatabase::Query> queries;
@@ -461,7 +465,7 @@ History::generateNewMessageQueries(const QString& friendPk, const QString& messa
         (peers)[friendPk] = peerId;
         queries += RawDatabase::Query(("INSERT INTO peers (id, public_key) "
                                        "VALUES (%1, '"
-                                       + friendPk + "');")
+                                       + friendPk.toString() + "');")
                                           .arg(peerId));
     }
 
@@ -479,7 +483,7 @@ History::generateNewMessageQueries(const QString& friendPk, const QString& messa
         (peers)[sender] = senderId;
         queries += RawDatabase::Query{("INSERT INTO peers (id, public_key) "
                                        "VALUES (%1, '"
-                                       + sender + "');")
+                                       + sender.toString() + "');")
                                           .arg(senderId)};
     }
 
@@ -581,9 +585,9 @@ RawDatabase::Query History::generateFileFinished(RowId id, bool success, const Q
     }
 }
 
-void History::addNewFileMessage(const QString& friendPk, const QString& fileId,
+void History::addNewFileMessage(const ToxPk& friendPk, const QString& fileId,
                                 const QString& fileName, const QString& filePath, int64_t size,
-                                const QString& sender, const QDateTime& time, QString const& dispName)
+                                const ToxPk& sender, const QDateTime& time, QString const& dispName)
 {
     if (historyAccessBlocked()) {
         return;
@@ -641,7 +645,7 @@ void History::addNewFileMessage(const QString& friendPk, const QString& fileId,
  * @param dispName Name, which should be displayed.
  * @param insertIdCallback Function, called after query execution.
  */
-void History::addNewMessage(const QString& friendPk, const QString& message, const QString& sender,
+void History::addNewMessage(const ToxPk& friendPk, const QString& message, const ToxPk& sender,
                             const QDateTime& time, bool isDelivered, QString dispName,
                             const std::function<void(RowId)>& insertIdCallback)
 {
@@ -823,7 +827,7 @@ QList<History::HistMessage> History::getUndeliveredMessagesForFriend(const ToxPk
  * @param parameter for search
  * @return date of the message where the phrase was found
  */
-QDateTime History::getDateWhereFindPhrase(const QString& friendPk, const QDateTime& from,
+QDateTime History::getDateWhereFindPhrase(const ToxPk& friendPk, const QDateTime& from,
                                           QString phrase, const ParameterSearch& parameter)
 {
     if (historyAccessBlocked()) {
@@ -899,7 +903,7 @@ QDateTime History::getDateWhereFindPhrase(const QString& friendPk, const QDateTi
                        "WHERE chat.public_key='%1' "
                        "AND %2 "
                        "%3")
-            .arg(friendPk)
+            .arg(friendPk.toString())
             .arg(message)
             .arg(period);
 

src/persistence/history.h
@@ -95,7 +95,7 @@ struct FileDbInsertionData
     FileDbInsertionData();
 
     RowId historyId;
-    QString friendPk;
+    ToxPk friendPk;
     QString fileId;
     QString fileName;
     QString filePath;
@@ -164,21 +164,21 @@ public:
     bool historyExists(const ToxPk& friendPk);
 
     void eraseHistory();
-    void removeFriendHistory(const QString& friendPk);
-    void addNewMessage(const QString& friendPk, const QString& message, const QString& sender,
+    void removeFriendHistory(const ToxPk& friendPk);
+    void addNewMessage(const ToxPk& friendPk, const QString& message, const ToxPk& sender,
                        const QDateTime& time, bool isDelivered, QString dispName,
                        const std::function<void(RowId)>& insertIdCallback = {});
 
-    void addNewFileMessage(const QString& friendPk, const QString& fileId,
+    void addNewFileMessage(const ToxPk& friendPk, const QString& fileId,
                            const QString& fileName, const QString& filePath, int64_t size,
-                           const QString& sender, const QDateTime& time, QString const& dispName);
+                           const ToxPk& sender, const QDateTime& time, QString const& dispName);
 
     void setFileFinished(const QString& fileId, bool success, const QString& filePath, const QByteArray& fileHash);
     size_t getNumMessagesForFriend(const ToxPk& friendPk);
     size_t getNumMessagesForFriendBeforeDate(const ToxPk& friendPk, const QDateTime& date);
     QList<HistMessage> getMessagesForFriend(const ToxPk& friendPk, size_t firstIdx, size_t lastIdx);
     QList<HistMessage> getUndeliveredMessagesForFriend(const ToxPk& friendPk);
-    QDateTime getDateWhereFindPhrase(const QString& friendPk, const QDateTime& from, QString phrase,
+    QDateTime getDateWhereFindPhrase(const ToxPk& friendPk, const QDateTime& from, QString phrase,
                                      const ParameterSearch& parameter);
     QList<DateIdx> getNumMessagesForFriendBeforeDateBoundaries(const ToxPk& friendPk,
                                                                const QDate& from, size_t maxNum);
@@ -187,8 +187,8 @@ public:
 
 protected:
     QVector<RawDatabase::Query>
-    generateNewMessageQueries(const QString& friendPk, const QString& message,
-                              const QString& sender, const QDateTime& time, bool isDelivered,
+    generateNewMessageQueries(const ToxPk& friendPk, const QString& message,
+                              const ToxPk& sender, const QDateTime& time, bool isDelivered,
                               QString dispName, std::function<void(RowId)> insertIdCallback = {});
 
 signals:
@@ -206,7 +206,7 @@ private:
     std::shared_ptr<RawDatabase> db;
 
 
-    QHash<QString, int64_t> peers;
+    QHash<ToxPk, int64_t> peers;
     struct FileInfo
     {
         bool finished = false;

src/persistence/profile.cpp
@@ -687,12 +687,11 @@ void Profile::onRequestSent(const ToxPk& friendPk, const QString& message)
         return;
     }
 
-    const QString pkStr = friendPk.toString();
     const QString inviteStr = Core::tr("/me offers friendship, \"%1\"").arg(message);
-    const QString selfStr = core->getSelfPublicKey().toString();
+    const ToxPk selfPk = core->getSelfPublicKey();
     const QDateTime datetime = QDateTime::currentDateTime();
     const QString name = core->getUsername();
-    history->addNewMessage(pkStr, inviteStr, selfStr, datetime, true, name);
+    history->addNewMessage(friendPk, inviteStr, selfPk, datetime, true, name);
 }
 
 /**

src/widget/widget.cpp
@@ -1713,7 +1713,7 @@ void Widget::removeFriend(Friend* f, bool fake)
         }
 
         if (ask.removeHistory()) {
-            Nexus::getProfile()->getHistory()->removeFriendHistory(f->getPublicKey().toString());
+            Nexus::getProfile()->getHistory()->removeFriendHistory(f->getPublicKey());
         }
     }
 

test/core/contactid_test.cpp
@@ -50,6 +50,7 @@ private slots:
     void copyTest();
     void dataTest();
     void sizeTest();
+    void hashableTest();
 };
 
 void TestContactId::toStringTest()
@@ -98,6 +99,15 @@ void TestContactId::sizeTest()
     GroupId id;
     QVERIFY(pk.getSize() == TOX_PUBLIC_KEY_SIZE);
     QVERIFY(id.getSize() == TOX_CONFERENCE_UID_SIZE);
+}
+
+void TestContactId::hashableTest()
+{
+    ToxPk pk1{testPkArray};
+    ToxPk pk2{testPk};
+    QVERIFY(qHash(pk1) == qHash(pk2));
+    ToxPk pk3{echoPk};
+    QVERIFY(qHash(pk1) != qHash(pk3));
 }
 
 QTEST_GUILESS_MAIN(TestContactId)