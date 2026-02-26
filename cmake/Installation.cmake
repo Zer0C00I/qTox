b265862d00be37e2052f0883ba1ee98d5d9e0408
Zer0C00I <you@example.com>
2026-02-26 11:39:38 +0200

    refactor: reorganize repository into a standard professional layout
    Move source-only libraries into src/ alongside the main source tree:
    - audio/ -> src/audio/
    - util/  -> src/util/
    
    Consolidate all non-code assets under a single assets/ directory:
    - img/, res/, resources/, themes/, smileys/, res.qrc -> assets/
    
    Rename directories to clarify their purpose:
    - platform/ -> packaging/  (OS/distro packaging scripts only)
    - doc/      -> docs/
    - tools/    -> scripts/
    
    Update all consumers of moved paths:
    - CMakeLists.txt, cmake/Installation.cmake, doxygen.conf
    - BUILD.bazel, src/BUILD.bazel, test/BUILD.bazel

--- cmake/Installation.cmake
+++ cmake/Installation.cmake
@@ -17,23 +17,23 @@ if(APPLE)
     ${BUNDLE_ID_OPTION}
     MACOSX_BUNDLE_BUNDLE_VERSION ${PROJECT_VERSION}
     MACOSX_BUNDLE_SHORT_VERSION_STRING ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}
-    MACOSX_BUNDLE_INFO_PLIST "${CMAKE_SOURCE_DIR}/platform/macos/Info.plist"
+    MACOSX_BUNDLE_INFO_PLIST "${CMAKE_SOURCE_DIR}/packaging/macos/Info.plist"
     MACOSX_BUNDLE TRUE
     WIN32_EXECUTABLE TRUE
   )
 
   set(BUNDLE_PATH "${CMAKE_BINARY_DIR}/${BINARY_NAME}.app")
 
-  list(APPEND PROJECT_ICONS "resources/images/icons/${BINARY_NAME}.icns")
-  list(APPEND PROJECT_ICONS "resources/images/icons/${BINARY_NAME}_profile.icns")
+  list(APPEND PROJECT_ICONS "assets/resources/images/icons/${BINARY_NAME}.icns")
+  list(APPEND PROJECT_ICONS "assets/resources/images/icons/${BINARY_NAME}_profile.icns")
 
   foreach(icon ${PROJECT_ICONS})
     install(FILES ${icon} DESTINATION ${BUNDLE_PATH}/Contents/Resources/)
   endforeach()
 
   # Search for createdmg. If it doesn't exist anywhere, download it.
-  if(EXISTS "${CMAKE_SOURCE_DIR}/platform/macos/createdmg")
-    set(CREATE_DMG "${CMAKE_SOURCE_DIR}/platform/macos/createdmg")
+  if(EXISTS "${CMAKE_SOURCE_DIR}/packaging/macos/createdmg")
+    set(CREATE_DMG "${CMAKE_SOURCE_DIR}/packaging/macos/createdmg")
   elseif(EXISTS "${CMAKE_SOURCE_DIR}/third_party/ci-tools/platform/macos/createdmg")
     set(CREATE_DMG "${CMAKE_SOURCE_DIR}/third_party/ci-tools/platform/macos/createdmg")
   elseif(EXISTS "${CMAKE_SOURCE_DIR}/../ci_tools/platform/macos/createdmg")
@@ -51,7 +51,7 @@ if(APPLE)
   )
 elseif(WIN32)
   install(CODE "
-  message(STATUS \"Installer can be created using platform/windows/${BINARY_NAME}64.nsi\")
+  message(STATUS \"Installer can be created using packaging/windows/${BINARY_NAME}64.nsi\")
   " COMPONENT Runtime
   )
 else()
@@ -62,18 +62,18 @@ else()
     LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
     RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
   )
-  install(FILES "platform/linux/${PROJECT_ORG}.${PROJECT_NAME}.appdata.xml" DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/metainfo")
-  install(FILES "platform/linux/${PROJECT_ORG}.${PROJECT_NAME}.desktop" DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/applications")
+  install(FILES "packaging/linux/${PROJECT_ORG}.${PROJECT_NAME}.appdata.xml" DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/metainfo")
+  install(FILES "packaging/linux/${PROJECT_ORG}.${PROJECT_NAME}.desktop" DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/applications")
 
   # Install application icons according to the XDG spec
   set(ICON_SIZES 14 16 22 24 32 36 48 64 72 96 128 192 256 512)
   foreach(size ${ICON_SIZES})
-    set(path_from "img/icons/${size}x${size}/${BINARY_NAME}.png")
+    set(path_from "assets/img/icons/${size}x${size}/${BINARY_NAME}.png")
     set(path_to "share/icons/hicolor/${size}x${size}/apps/")
     install(FILES ${path_from} DESTINATION ${path_to})
   endforeach(size)
 
   # process the icon, compress if enabled
-  set(SVG_SRC "${CMAKE_SOURCE_DIR}/img/icons/${BINARY_NAME}.svg")
+  set(SVG_SRC "${CMAKE_SOURCE_DIR}/assets/img/icons/${BINARY_NAME}.svg")
   install(FILES "${SVG_SRC}" DESTINATION "share/icons/hicolor/scalable/apps")
 endif()
